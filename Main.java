da280ec81598bee085e3107a7f5562a4419eb62e
VaughanGalvin <76697320+VaughanGalvin@users.noreply.github.com>
2021-01-29 00:18:22 +0300

    fixes #1 method added

--- Main.java
+++ Main.java
@@ -2,10 +2,9 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+     long bonus = service.calculate(1000, true);
 
-    service.calculate(amount, registered);
+    System.out.println(bonus);
   }
 }
 
